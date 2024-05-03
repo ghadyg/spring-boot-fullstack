1bd25f383eace5b6362783ad5b720f96902323ca
ghadyg <ghoshghady22@gmail.com>
2024-05-03 18:45:49 +0300

    commit

--- backend/src/main/java/com/fullStackCourse/customer/CustomerRegistrationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.fullStackCourse.customer;
-
-public record CustomerRegistrationRequest(
-        String name,
-        String email,
-        Integer age
-) {
-}
--- backend/src/main/java/com/fullStackCourse/customer/CustomerUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.fullStackCourse.customer;
-
-public record CustomerUpdateRequest (
-        String name,
-        String email,
-        Integer age
-) {
-}
-
